f691af919e0d433589a8635bdb74d6f20e644bd1
dccmmtop <dccmmtop@gmail.com>
2017-07-16 10:16:17 +0800

    123

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-	validates :name,presence: true,length:{maximum: 10,minimum:2}
+  validates :name, presence: true, length: { maximum: 10, minimum: 2 }
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-	def setup
-		@base_title="Ruby on rails tutorial sample app"
-		
-	end
-  test "should get home" do
-    get static_pages_home_url
+  def setup
+    @base_title = 'Ruby on rails tutorial sample app'
+  end
+  test 'should get home' do
+    get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select 'title', "Home | #{@base_title}"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
-  test "should get about" do
-  	get static_pages_about_url
-  	assert_response :success
-  	assert_select "title", "About | #{@base_title}"
+  test 'should get about' do
+    get about_path
+    assert_response :success
+    assert_select 'title', "About | #{@base_title}"
   end
-  test "should get root" do
-  	get root_url
-  	assert_response :success
+  test 'should get root' do
+    get root_path
+    assert_response :success
   end
-
+  test 'should get contact' do
+    get contact_path
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
 
 end
