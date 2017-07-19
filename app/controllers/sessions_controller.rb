e8a27da399304a085eaba7637ea169acf03f797d
dccmmtop <dccmmtop@gmail.com>
2017-07-19 10:18:01 +0800

    add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,5 +12,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    session[:user_id]=nil
+    
+
+   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success]="Welcome to the Sameple App!"
       redirect_to @user
     else
