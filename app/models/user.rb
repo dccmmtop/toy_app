1e6ccd6096e7e0386f4a206ea75d47651782b8fc
dccmmtop <dccmmtop@gmail.com>
2017-07-19 11:17:51 +0800

    login two

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # attr_accessor :name,:email
+  before_save {self.email=email.downcase}
   EMAIL_FORMAT= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50, minimum: 2 }
   validates :email, presence:true,length:{ maximum:50},format:{with:EMAIL_FORMAT},uniqueness:{case_sensitive: false}
