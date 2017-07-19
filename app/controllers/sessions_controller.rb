305e77f6cf6b685fa1bd0e283e4de7f83c3fa901
dccmmtop <dccmmtop@gmail.com>
2017-07-19 09:05:01 +0800

    login onr

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    params[:session][:email]="#" if !params[:session][:email]
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
